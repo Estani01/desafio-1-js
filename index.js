fd067f55b3829c0ec73e7d550aa2a0407a018a3a
estanislao01 <estani03@gmail.com>
2022-06-15 16:18:27 -0300

    inicial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+for (let index=1 ; index<100; index++){
+        let nombre= prompt ("ingrese su nombre");
+        let apellido = prompt ("ingrese su apellido")
+        alert ("tu turno es " + index + " estimado  " + nombre +" " + apellido  )
+        if (index===4){
+            break;
+           
+            
+        }
+       
+        
+}
+alert ("RECORDAMOS A LOS PACIENTES QUE EL DIA 24/08 ESTAREMOS DANDO OTROS 4 TURNOS")
